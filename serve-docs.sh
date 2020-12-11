f9055adf2f72bd3cc9697039a485252ff75e7358
queil <queil@users.noreply.github.com>
2020-12-11 21:52:17 +0000

    Added reset root URL after finishing serve

--- serve-docs.sh
+++ serve-docs.sh
@@ -2,3 +2,4 @@
 
 ./build-fsdocs.sh "http://localhost:8089/"
 docker run -p 8089:8089 --rm -it -v ~/.ssh:/root/.ssh -v ${PWD}:/docs squidfunk/mkdocs-material serve -a 0.0.0.0:8089
+./build-fsdocs.sh "https://queil.github.io/yzl/"
