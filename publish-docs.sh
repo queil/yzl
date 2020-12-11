3e621240207264ca58f83b4d6060c44b7a85ef28
queil <queil@users.noreply.github.com>
2020-12-11 21:48:51 +0000

    Docs tweaks

--- publish-docs.sh
+++ publish-docs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./build-fsdocs "https://queil.github.io/yzl/"
+./build-fsdocs.sh "https://queil.github.io/yzl/"
 docker run -p 8089:8089 --rm -it -v ~/.ssh:/root/.ssh -v ${PWD}:/docs squidfunk/mkdocs-material gh-deploy
