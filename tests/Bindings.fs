ae59708eb8654ad222a4cd35ec4bdf5b4732d180
queil <queil@users.noreply.github.com>
2021-08-01 12:56:45 +0100

    Fixing tests

--- tests/Bindings.fs
+++ tests/Bindings.fs
@@ -4,34 +4,12 @@ module Bindings =
 
   open Expecto
   open Yzl.Core
-  
+  open Yzl.Bindings.Kustomize
 
-  [<AutoOpen>]
-  module Test2 =
     
     
 
     
-    type Kustz(?images: Kustz, ?other: int) = 
-      member x.Images = images
-    and
-     Kustomization =
-     private
-     | ApiVersion of ApiVersion<Kustomization>
-     | Images of Image seq
-    and ApiVersion<'a> = 'a option -> string
-    and Image =
-     | Name of Name<Image>
-     | ApiVersion of ApiVersion<Image>
-    and Name<'a> = 'a option -> string
- 
-    let apiVersion (value:ApiVersion<'a>) = 
-      fun z ->
-        let x = value None
-        Yzl.str "apiVersion" x
-   
-    let images (x: (list<(Image option -> Yzl.NamedNode)>)) = 
-      x |> List.map (fun z -> z None)
     
     
 
@@ -40,25 +18,8 @@ module Bindings =
     testList "Bindings" [
       
       test "Kustomize" {
-        let kustomization (x: (list<(Kustomization option -> Yzl.NamedNode)>)) = 
-          ! ((x |> List.map (fun z -> z None ) ))
         
-        let ii = ! 2
-        let y = ii := "test"
-           
-        
-        
-        let yaml = 
-          kustomization [
-            apiVersion (fun x -> "test")
-            // images [
-            //   ! [
-            //     fun x -> Yzl.map "image" []
-            //   ]
-            // ]
-            
-            ]
         let expected = ""
-        "Rendering failed" |> Expect.equal (yaml |> Yzl.render) expected  
+        "Rendering failed" |> Expect.equal (!5 |> Yzl.render) expected  
       }
     ]
