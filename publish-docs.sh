00d11db61d049c1a48db3def0e13b41962e67be1
queil <queil@users.noreply.github.com>
2021-10-09 21:24:46 +0100

    Disable publish docs for now

--- publish-docs.sh
+++ publish-docs.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
+# if it fails make sure chown root:$USER ~/.ssh/config
 ./build-fsdocs.sh "https://queil.github.io/yzl/"
-#podman: docker run -p 8089:8089 --rm -it -v ~/.ssh:/root/.ssh:Z -v ${PWD}:/docs:Z docker.io/squidfunk/mkdocs-material gh-deploy
-docker run -p 8089:8089 --rm -it -v ~/.ssh:/root/.ssh:ro -v ${PWD}:/docs docker.io/squidfunk/mkdocs-material gh-deploy
+docker run --rm -it -v ~/.ssh:/root/.ssh:Z -v ${PWD}:/docs:Z docker.io/squidfunk/mkdocs-material gh-deploy
